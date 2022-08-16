83effbcc4c732a9634bfc33c3d86ae1f484a4994
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-16 16:39:20 +1000

    initial page structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { categories } from "./utils/dummy";
+
 const App = () => {
-  return <div>Yes</div>;
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div key={category.id} className="category-container">
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/utils/dummy.js
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Apple",
+  },
+  {
+    id: 2,
+    title: "Lemon",
+  },
+  {
+    id: 3,
+    title: "Orange",
+  },
+  {
+    id: 4,
+    title: "Banana",
+  },
+  {
+    id: 5,
+    title: "Strawberry",
+  },
+];
