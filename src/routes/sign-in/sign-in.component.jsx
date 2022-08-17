eca186a70fb05ba0f7d6253386e2d69dc7c5a018
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-17 17:02:43 +1000

    method to create user with redirect link

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   createUserDocumentFromAuth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(
+    () => async () => {
+      const res = await getRedirectResult(auth);
+      if (res) {
+        const userDocRef = await createUserDocumentFromAuth(res.user);
+        console.log(userDocRef);
+      }
+    },
+    []
+  );
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
     console.log(userDocRef);
   };
+
   return (
     <div>
       SignIn
       <button onClick={logGoogleUser}>Sign In With Google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google redirect
+      </button>
     </div>
   );
 };
