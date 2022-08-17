6e5c798c1e6d112776e9e61b22717ade708c3eed
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-17 11:02:15 +1000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import Categories from "./components/categories/categories.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  return <Categories />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
