54dd021580324b765d69ff00999afbed45ebafc0
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-18 15:22:02 +1000

    change submit to button for google sign in button so it doesn't submit by default

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -73,7 +73,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button buttonType="google" type="submit" onClick={signInWithGoogle}>
+          <Button buttonType="google" type="button" onClick={signInWithGoogle}>
             Google Sign In
           </Button>
         </div>
