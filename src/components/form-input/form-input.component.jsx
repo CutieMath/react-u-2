501cdc260b30f74760d21403a28dde86c6a8c5b9
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-18 11:49:18 +1000

    add styling into sign up form

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,10 +1,19 @@
+import "./form-input.styles.scss";
 import React from "react";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input {...otherProps} />
+    <div className="group">
+      <input className="form-input" {...otherProps} />
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
     </div>
   );
 };
