2e137f94d49df3fc9bdaeda4a09282441fd96073
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-18 10:07:58 +1000

    understand name and value in forms

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+
+const defaultFormState = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const SignUpForm = () => {
+  const [formData, setFormData] = useState(defaultFormState);
+  const { displayName, email, password, confirmPassword } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...defaultFormState, [name]: value }); // update the ralated field with the new value
+  };
+
   return (
     <div>
       <h2>Sign up with your email and password</h2>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
 
         <label>Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
 
         <button type="submit">Sign Up</button>
       </form>
