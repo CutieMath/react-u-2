a0324da69201910b0ae7d1c5cac0562569d009d5
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-18 15:30:32 +1000

    finish styling for the auth page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,34 +1,37 @@
 import React, { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-import {
-  auth,
-  createUserDocumentFromAuth,
-  signInWithGooglePopup,
-  signInWithGoogleRedirect,
-} from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import "./authentication.styles.scss";
+// Used for google redirect
+// import {
+//   auth,
+//   createUserDocumentFromAuth,
+//   signInWithGooglePopup,
+//   // signInWithGoogleRedirect,
+// } from "../../utils/firebase/firebase.utils";
+// import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
-  useEffect(
-    () => async () => {
-      const res = await getRedirectResult(auth);
-      if (res) {
-        const userDocRef = await createUserDocumentFromAuth(res.user);
-        console.log(userDocRef);
-      }
-    },
-    []
-  );
+  // Used for google redirect
+  // useEffect(
+  //   () => async () => {
+  //     const res = await getRedirectResult(auth);
+  //     if (res) {
+  //       const userDocRef = await createUserDocumentFromAuth(res.user);
+  //       console.log(userDocRef);
+  //     }
+  //   },
+  //   []
+  // );
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef);
-  };
+  // const logGoogleUser = async () => {
+  //   const { user } = await signInWithGooglePopup();
+  //   const userDocRef = await createUserDocumentFromAuth(user);
+  //   console.log(userDocRef);
+  // };
 
   return (
-    <div>
+    <div className="authentication-container">
       <SignInForm />
       <SignUpForm />
       {/* 
