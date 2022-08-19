efd00d59dfa79d19fc2e1e120c37908701fce5ea
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-19 14:18:40 +1000

    add styling to cards

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import { ProductsContext } from "../../context/products.context";
+import "./shop.styles.scss";
 
 const Shop = () => {
   const { productsData } = useContext(ProductsContext);
   return (
-    <div>
+    <div className="products-container">
       {productsData.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
