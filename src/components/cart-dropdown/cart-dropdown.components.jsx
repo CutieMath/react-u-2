f0d5ca7477d38331d4ffe19706c6f481290976c4
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-19 15:50:09 +1000

    method to increase product quantity in cart dropdown. Put the logic in Cart dropdown context

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.components";
 
 const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
+        <CartItem />
         <Button>Girl</Button>
       </div>
     </div>
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -3,10 +3,56 @@ import { useState, createContext } from "react";
 export const CartContext = createContext({
   showCartDropdown: false,
   setShowCartDropdown: () => {},
+  cartItems: [],
+  addItem: () => {},
 });
+
+/*
+product
+{
+  id,
+  name, 
+  price, 
+  imageUrl,
+}
+
+Cart Item
+{
+  id,
+  name,
+  price,
+  imageUrl
+  quantity,
+}
+*/
+
+const helperFunctionForAddingCartItem = (cartItems, productToAdd) => {
+  // Check if the product is already in the cart
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  // If it is, then increase the quantity by 1
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // If it is not, then add the product to the cart
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartProvider = ({ children }) => {
   const [showCartDropdown, setShowCartDropdown] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (product) => {
+    setCartItems(helperFunctionForAddingCartItem(cartItems, product));
+  };
+
   const value = { showCartDropdown, setShowCartDropdown };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
