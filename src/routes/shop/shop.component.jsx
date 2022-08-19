fc9c7ec31bf9af504340cf49a5a4e7e0d06bed7d
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-19 12:17:56 +1000

    populate shop components

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { shopData } from "../../utils/dummy";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      {shopData.map((product) => (
+        <div key={product.id}>
+          <h1>{product.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
