728e65b71759f1241f14e8f1074950c12b0e8668
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-24 10:32:47 +1000

    display categoryPreview in the front-end

--- src/components/category-preview/category-preview.components.jsx
+++ src/components/category-preview/category-preview.components.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ProductCard from "../product-card/product-card.component";
 import "./category-preview.styles.scss";
 
-const CategoryPreview = ({ title, products }) => {
+const CategoryPreview = ({ title, product }) => {
   return (
     <div className="category-preview-container">
       <h2>
         <span className="title">{title.toUpperCase()}</span>
       </h2>
       <div className="preview">
-        {products
+        {product
           .filter((_, index) => index < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,18 @@
 import React, { useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.components";
 import { CategoriesContext } from "../../context/categories.context";
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
-      {Object.keys(categoriesMap).map((title, index) => (
-        <React.Fragment key={index}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </React.Fragment>
-      ))}
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title, index) => {
+        const products = categoriesMap[title];
+        return <CategoryPreview key={index} title={title} product={products} />;
+      })}
       <div className="products-container"></div>
-    </>
+    </div>
   );
 };
 
