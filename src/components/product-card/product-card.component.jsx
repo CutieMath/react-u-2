eed2d18c8e099fe137876ae4ae52f7700a48acea
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-24 14:45:03 +1000

    add button into styled components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./product-card.styles.scss";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
 
 const ProductCard = ({ product }) => {
   const { name, imageUrl, price } = product;
   const { addItemToCart } = useContext(CartContext);
-  const addPoroductToCart = () => addItemToCart(product);
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -16,7 +16,10 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted" onClick={addPoroductToCart}>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
         Add Cart
       </Button>
     </div>
