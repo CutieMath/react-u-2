3696264a6ae7d973d710c9e5aa4752cbfa43d224
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-24 11:29:44 +1000

    add safe guard for data returned from async functions

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.components";
+import Category from "../category/category.components";
 import "./shop.styles.scss";
 
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
