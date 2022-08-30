15bbb235b13113853bc8c2dbe7f8837ffc6ebafd
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-30 12:09:23 +1000

    get the userRedux to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Home from "./routes/home/home.component";
 import Navbar from "./routes/navbar/navbar.component";
 import Shop from "./routes/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component";
 import CheckOut from "./routes/checkout/checkout.component";
+import { setCurrentUser } from "./store/user/user.action";
+
+import { useEffect } from "react";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "./utils/firebase/firebase.utils";
 
 const App = () => {
+  // The dispatch value will not be changed
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // onSubscribe when component unmount
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user)); // if the user sign out, the call back will return null
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
