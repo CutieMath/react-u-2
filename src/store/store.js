99f27dd6d6f19cfcd417e07e38d42d43ccae65c3
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-30 13:51:24 +1000

    add more comments for redux create store method

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,9 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+// Runs before an action hits a reducer
 const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
+// Runs before an action hits a reducer
+
 export const store = createStore(rootReducer, undefined, composeEnhancers);
