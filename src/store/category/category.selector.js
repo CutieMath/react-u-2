aa0041fb404719dc540649600206259e59d3cd93
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-06 11:47:04 +1000

    understand re-selector and memoization in redux

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -1,8 +1,28 @@
-// Selector is where we transforms data and do business logic
+// 1. Selector is where we transforms data and do business logic
 // When getting data from an API, we only get the most pure format of them.
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((accumulator, category) => {
-    const { title, items } = category;
-    accumulator[title.toLowerCase()] = items;
-    return accumulator;
-  }, {});
+
+// 2. Use reselect to cache the value of previous selector => Memoization
+// This is to prevent components from re-rendering when the data is the same
+
+import { createSelector } from "reselect";
+
+// Create initial selector
+const selectInitialCategoryReducer = (state) => state.categories;
+
+// Create memoized selector
+export const selectMemoizedCategories = createSelector(
+  [selectInitialCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// Create second memoized selector which uses the first memoized selector
+export const selectCategoriesMap = createSelector(
+  [selectMemoizedCategories],
+  (categories) => {
+    return categories.reduce((accumulator, category) => {
+      const { title, items } = category;
+      accumulator[title.toLowerCase()] = items;
+      return accumulator;
+    }, {});
+  }
+);
