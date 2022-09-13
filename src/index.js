c2970ac02d121d923a95a776152f00e7c036ef4a
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-13 13:49:47 +1000

    add persist data to the project to avoid checkout page render to empty on refresh

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 // Create our own logger middleware
 // const loggerMiddleware = (store) => (next) => (action) => {
@@ -14,9 +16,19 @@ import { rootReducer } from "./root-reducer";
 //   console.log("Next State", store.getState());
 // };
 
+// Persist
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["user"], // may conflict with local storage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+// Persist
+
 // Catch actions before a reducer is hit
 const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 // Catch actions before a reducer is hit
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+export const persistor = persistStore(store);
