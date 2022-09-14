9727a7a6338ad9deb990b3ec725afc37ad1bc7b5
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-14 13:59:10 +1000

    add spinner for category component

--- src/routes/category/category.components.jsx
+++ src/routes/category/category.components.jsx
@@ -3,10 +3,11 @@ import { CategoriesContext } from "../../context/categories.context";
 import { useParams } from "react-router-dom";
 import "./category.styles.scss";
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -16,6 +17,7 @@ const Category = () => {
   return (
     <>
       <h2 className="category-title-category-page">{category}</h2>
+      {loading && <Spinner />}
       <div className="category-container">
         {products && // Safe guard for data returned from async function
           products.map((product) => (
