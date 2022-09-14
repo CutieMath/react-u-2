7d5eede5deb043756d9e341a89b0651750828dad
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-14 11:18:27 +1000

    setup apollo client

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user.context";
 import { CategoriesProvider } from "./context/categories.context";
 import { CartProvider } from "./context/cart.context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const client = new ApolloClient({
+  uri: "https://crwn-clothing.com",
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
