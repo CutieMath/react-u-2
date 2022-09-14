c5c69065c7c9030d5614424d61291de3d1cf808d
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-14 11:27:32 +1000

    create graphql call inside categories context folder

--- src/context/categories.context.jsx
+++ src/context/categories.context.jsx
@@ -1,20 +1,42 @@
 import { React, useState, createContext, useEffect } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+import { gql, useQuery } from "@apollo/client";
 
 export const CategoriesContext = createContext({
   categoriesMap: [],
 });
 
+const COLLECTIONS_QUERY = gql`
+  query GetCollections {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS_QUERY);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoryProducts = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-    getCategoryProducts();
-  }, []);
+  console.log(loading);
+  console.log(data);
+
+  // Replace the following code with Apollo client
+  // We are now having a better way to fetch data from the server
+  // useEffect(() => {
+  //   const getCategoryProducts = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments();
+  //     setCategoriesMap(categoryMap);
+  //   };
+  //   getCategoryProducts();
+  // }, []);
 
   const value = { categoriesMap };
   return (
